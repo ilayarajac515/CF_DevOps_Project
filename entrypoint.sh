dd8c7abf407de275df1c9779f2d6e16a681242ae
ilayarajac515 <ilayarajac515@gmail.com>
2024-12-12 13:55:20 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start ColdFusion in the background
+/opt/coldfusion/cfusion/bin/coldfusion start
+
+# Wait for ColdFusion to be fully started
+echo "Waiting for ColdFusion to start..."
+sleep 60
+
+# Execute the datasource setup script
+echo "Setting up datasource..."
+curl -X POST http://localhost:8500/WEB-INF/datasource.cfm
+
+# Keep the container running
+tail -f /opt/coldfusion/cfusion/logs/coldfusion-out.log
